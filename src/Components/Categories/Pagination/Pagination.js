31775e17bd70bb8a8d1f4578c9721e2157aef273
Valentyna Boiko <boiko.vtyna@gmail.com>
2022-05-27 17:49:01 +0200

    Added Pagination.

--- src/Components/Categories/Pagination/Pagination.js
+++ src/Components/Categories/Pagination/Pagination.js
@@ -3,9 +3,14 @@ import './Pagination.css';
 import { firebaseUrl } from '../../../configs';
 import ItemCard from '../../ItemCard/ItemCard';
 
+let pageNext = 1;
+
 const Pagination = () => {
 
-    const [page, setPage] = useState(0);
+    let [page, setPage] = useState(1);
+    let [startFromItem, setStartFromItem] = useState(0);
+    let [update, setUpdate] = useState(false);
+
     const [allGoods, setAllGoods] = useState([]);
 
     const urls = ['/cats/food/dry', '/cats/food/wet', '/cats/toilets_fillers', '/cats/accessories', '/cats/vitamins',
@@ -36,14 +41,42 @@ const Pagination = () => {
 
     recursion(allGoods);
 
-    console.log('All goods', allGoods);
-    console.log('Array elems', goodsArr);
+    const limitItemsPerOnePage = 8;
+    const countPages = Math.ceil(goodsArr.length / limitItemsPerOnePage);
+    const startFromItemTheLastPage = goodsArr.length - (goodsArr.length % Math.floor(goodsArr.length / limitItemsPerOnePage)); 
+
+    const itemsForPage = goodsArr.slice(startFromItem, startFromItem + limitItemsPerOnePage);
+
+    const handlerNextPage = () => {
+        if(page < countPages) { 
+            setPage(++pageNext);
+            setStartFromItem(startFromItem + limitItemsPerOnePage);
+        } else {
+            setStartFromItem(startFromItemTheLastPage)
+        }
+        setUpdate(!update); 
+    }
+ 
+    const handlerPrevPage = () => {
+        if(page > 1) { 
+            setPage(--pageNext);
+            setStartFromItem(startFromItem - limitItemsPerOnePage);
+        } else {
+            setStartFromItem(0)
+        }
+        setUpdate(!update); 
+    }
 
     return ( 
         <div className='pagination-page'>
              <h2>All goods:</h2>
+             <div className='pagination-panel'>
+                 <button className='prev-next' onClick={handlerPrevPage}> prev &lt;</button>
+                    <p> Page: <strong>{page}</strong> from <strong>{countPages}</strong></p>
+                 <button className='prev-next' onClick={handlerNextPage}>&gt; next</button>
+             </div>
              <div className='pagination'>
-                {goodsArr.map(elem => {
+                {itemsForPage.map(elem => {
                     return <ItemCard key={elem.id} data={elem} />
                 })}
             </div> 
